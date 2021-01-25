99846cc8c20631b9fa93fd275d1658b0e27b79e6
filipecosta90 <filipecosta.90@gmail.com>
2021-01-25 17:14:35 +0000

    [wip] wip jedis

--- src/main/java/com/redislabs/ClientThread.java
+++ src/main/java/com/redislabs/ClientThread.java
@@ -4,23 +4,27 @@ import com.google.common.util.concurrent.RateLimiter;
 import com.redislabs.redisgraph.RedisGraph;
 import com.redislabs.redisgraph.ResultSet;
 import com.redislabs.redisgraph.Statistics;
+import com.redislabs.redisgraph.impl.api.RedisGraphCommand;
 import com.sun.org.apache.xalan.internal.lib.ExsltStrings;
 import org.HdrHistogram.*;
 import com.google.common.util.concurrent.RateLimiter;
+import redis.clients.jedis.JedisPool;
+
+import java.util.List;
 
 public class ClientThread extends Thread {
     private final int requests;
-    private final RedisGraph rg;
+    private final JedisPool rg;
     private final String query;
     private final String key;
     private final Histogram histogram;
     private final Histogram graphInternalHistogram;
     private final RateLimiter rateLimiter;
 
-    ClientThread(RedisGraph rg, Integer requests, String key, String query, ConcurrentHistogram histogram, ConcurrentHistogram graphInternalHistogram) {
+    ClientThread(JedisPool pool, Integer requests, String key, String query, ConcurrentHistogram histogram, ConcurrentHistogram graphInternalHistogram) {
         super("Client thread");
         this.requests = requests;
-        this.rg = rg;
+        this.rg = pool;
         this.query = query;
         this.key = key;
         this.histogram = histogram;
@@ -28,10 +32,10 @@ public class ClientThread extends Thread {
         this.rateLimiter = null;
     }
 
-    ClientThread(RedisGraph rg, Integer requests, String key, String query, ConcurrentHistogram histogram, ConcurrentHistogram graphInternalHistogram, RateLimiter perClientRateLimiter) {
+    ClientThread(JedisPool pool, Integer requests, String key, String query, ConcurrentHistogram histogram, ConcurrentHistogram graphInternalHistogram, RateLimiter perClientRateLimiter) {
         super("Client thread");
         this.requests = requests;
-        this.rg = rg;
+        this.rg = pool;
         this.query = query;
         this.key = key;
         this.histogram = histogram;
@@ -46,11 +50,11 @@ public class ClientThread extends Thread {
                 rateLimiter.acquire(1);
             }
             long startTime = System.nanoTime();
-            ResultSet resultSet = rg.query(key, query);
+            List<Object> rawResponse = (List<Object>) rg.getResource().sendCommand(RedisGraphCommand.QUERY,key, query,"--compact");
             long durationMicros = (System.nanoTime() - startTime) / 1000;
-            String splitted = resultSet.getStatistics().getStringValue(Statistics.Label.QUERY_INTERNAL_EXECUTION_TIME).split(" ")[0];
-            double internalDuration = Double.parseDouble(splitted) * 1000;
-            graphInternalHistogram.recordValue((long) internalDuration);
+//            String splitted = resultSet.getStatistics().getStringValue(Statistics.Label.QUERY_INTERNAL_EXECUTION_TIME).split(" ")[0];
+//            double internalDuration = Double.parseDouble(splitted) * 1000;
+//            graphInternalHistogram.recordValue((long) internalDuration);
             histogram.recordValue(durationMicros);
         }
 //        System.out.println("My thread run is over");
