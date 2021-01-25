b63e8c4d501400bbd9b1471d1b394a6e176d3df9
filipecosta90 <filipecosta.90@gmail.com>
2021-01-25 17:32:50 +0000

    [fix] fix jedis usage

--- src/main/java/com/redislabs/ClientThread.java
+++ src/main/java/com/redislabs/ClientThread.java
@@ -51,9 +51,9 @@ public class ClientThread extends Thread {
                 rateLimiter.acquire(1);
             }
             long startTime = System.nanoTime();
-            try ( Jedis jedis = rg.getResource() ){
-                List<Object> rawResponse = (List<Object>)jedis.sendCommand(RedisGraphCommand.QUERY,key, query,"--compact");
-            }
+            Jedis jedis = rg.getResource();
+            List<Object> rawResponse = (List<Object>)jedis.sendCommand(RedisGraphCommand.QUERY,key, query,"--compact");
+            jedis.close();
             long durationMicros = (System.nanoTime() - startTime) / 1000;
 //            String splitted = resultSet.getStatistics().getStringValue(Statistics.Label.QUERY_INTERNAL_EXECUTION_TIME).split(" ")[0];
 //            double internalDuration = Double.parseDouble(splitted) * 1000;
